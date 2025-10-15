c30d4a6baae0850c0be044aaf069b2d6b93cabe1
Michał Kołodziejczyk <michal94mk@o2.pl>
2025-10-15 09:46:25 +0200

    feat(controllers): add Post and Comment controllers with CRUD operations

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,38 @@
 <?php
 
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+// Redirect home to posts
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('posts.index');
 });
 
+// Posts routes - public routes
+Route::get('posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
+
+// Posts routes - authenticated routes
+Route::middleware('auth')->group(function () {
+    Route::get('posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::post('posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('posts/{post}', [PostController::class, 'update'])->name('posts.update');
+    Route::delete('posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+});
+
+// Comments routes
+Route::post('posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store');
+Route::delete('comments/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
+
+// Dashboard (optional - can be removed if not needed)
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return redirect()->route('posts.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Profile routes
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
